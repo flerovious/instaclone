3cb2d14528360339a8b460bfeb4a241264b298fd
flerovious <85042815+flerovious@users.noreply.github.com>
2022-07-02 23:37:30 +0800

    Add Navbar

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,7 @@
-import { expect, test } from '@playwright/test';
+// Commented for future reference when writing tests
+// import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-  await page.goto('/');
-  expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
-});
+// test('index page has expected h1', async ({ page }) => {
+//   await page.goto('/');
+//   expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+// });
